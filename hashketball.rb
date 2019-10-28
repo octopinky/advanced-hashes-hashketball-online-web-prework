142756a82bc81854b0acffcf180f8509114efca5
Olga  Chupp <olga.chupp@gmail.com>
2019-10-28 09:36:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,24 @@
 require 'pry'
-def hashketball
-  def game_hash 
-  end
-  binding.pry
+def game_hash
+  {
+    away: { 
+            team_name: ' ',
+            colors: %w[ ],
+            players: [
+              { player_name: 'Jeff Adrien',
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slam_dunks: 2 }, },
+              { },
+              { },
+              { },
+              { }
+            ] },
+    home: { }
+  }
 end
